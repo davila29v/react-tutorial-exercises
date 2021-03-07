4aff4565993195413aea43b1a47d236374f07e3e
davila29v <davila29v@hotmail.com>
2021-03-07 02:11:37 +0000

    ya casi

--- exercises/04-conditional-rendering/index.js
+++ exercises/04-conditional-rendering/index.js
@@ -4,13 +4,18 @@ import PropTypes from "prop-types";
 
 const Alert = props => {
 	//add the condition inside this function
-
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+	console.log("Will show me the property show value: ", props.show);
+	if (props.show == false) {
+		return null;
+	} else {
+		return (
+			<div className="alert alert-danger" role="alert">
+				{props.text}
+			</div>
+		);
+	}
 };
+
 Alert.propTypes = {
 	color: PropTypes.string,
 	text: PropTypes.string
